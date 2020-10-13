10eafecf01f8f7c2da52875f678beff772d55d85
isaackumi <isaac.kumi@ashesi.edu.gh>
2020-10-13 15:44:54 +0000

    print_r

--- db_cred.php
+++ db_cred.php
@@ -2,8 +2,8 @@
 <?php
 //Database credentials
 define("DATABASE", "myblog");
-define("SERVER", "http://13.68.189.1");
-// define("SERVER", "localhost");
+// define("SERVER", "13.68.189.1");
+define("SERVER", "localhost");
 define("USERNAME", "root");
 // define("PASSWD", "");
 define("PASSWD", "0548atlove");
